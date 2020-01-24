f46e2d2efb204552846dfdf003e29f91d5262a2e
Luis Segoviano <luissegoviano.14@gmail.com>
2020-01-24 14:54:10 -0600

    Initial Commit

---
+++ Justo Technical Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Justo Technical Test
+//
+//  Created by Luis Segoviano on 24/01/20.
+//  Copyright © 2020 Luis Segoviano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
